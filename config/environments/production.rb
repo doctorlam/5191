035e883c645681582fab4c44971905a7d4060e72
Christopher Lam <ChrisLam@ChristophersAir.home>
2016-08-21 14:18:59 -0500

    setting up amazin s3

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -80,4 +80,13 @@ Rails.application.configure do
   #Required for devise. Remember to change localhost:3000 to application host
   config.action_mailer.default_url_options = { host: 'http://course-website.herokuapp.com' }
 
+config.paperclip_defaults = {
+  storage: :s3,
+  s3_credentials: {
+    bucket: ENV.fetch('S3_BUCKET_NAME'),
+    access_key_id: ENV.fetch('AWS_ACCESS_KEY_ID'),
+    secret_access_key: ENV.fetch('AWS_SECRET_ACCESS_KEY'),
+    s3_region: ENV.fetch('AWS_REGION'),
+  }
+}
 end
