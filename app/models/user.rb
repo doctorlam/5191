c087d2b9756faee658fb24592b171fd491656b69
Christopher Lam <ChrisLam@dynamic-10-125-150-120.dynamic.unt.edu>
2016-08-29 14:39:14 -0500

    added takeaways

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,7 +9,6 @@ class User < ActiveRecord::Base
    has_many :projects, dependent: :destroy 
    has_many :abouts, dependent: :destroy
    has_many :submissions
-
    has_attached_file :avatar, :styles => { :medium => "300x300>", :thumb => "100x100#" }, :default_url => "missing.png"
   validates_attachment_content_type :avatar, :content_type => /\Aimage\/.*\Z/
   acts_as_commontator
