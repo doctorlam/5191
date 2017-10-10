007e97ded8f490cfb53667d18fac3c11698035ab
Chris <chris.lam.unt@gmail.com>
2017-10-10 16:09:56 -0500

    trying to add assignment

--- app/mailers/homework_mailer.rb
+++ app/mailers/homework_mailer.rb
@@ -1,9 +1,9 @@
 class HomeworkMailer < ApplicationMailer
 	 default from: "chris.lam@unt.edu"
   
-  	def sample_email(submission_user)
+  	def sample_email(submission_user, submission_assignment)
   		@submission_user = submission_user
-  	
+  		@submission_assignment = submission_assignment
     	mail(to: submission_user.email, subject: 'An assignment has been graded!')
   end
 end
